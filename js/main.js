7411c71601b6f759a5b5150c6e2aaf178f4d5759
Andi <andi_haller@hotmail.com>
2023-02-10 15:16:43 -0300

    Navbar and Dark mode

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Dark Light switch
+const switchModeBtn = document.getElementById("cb1");
+const body = document.body;
+
+switchModeBtn.addEventListener("click", function() {
+	body.classList.toggle("day-background");
+});
+
+// Navbar
+const navbarToggle = navbar.querySelector("#navbar-toggle");
+const navbarMenu = document.querySelector("#navbar-menu");
+const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
+let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
+
+const toggleNavbarVisibility = () => {
+  isNavbarExpanded = !isNavbarExpanded;
+  navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
+};
+
+navbarToggle.addEventListener("click", toggleNavbarVisibility);
+
+navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
+navbarMenu.addEventListener("click", toggleNavbarVisibility);
+
