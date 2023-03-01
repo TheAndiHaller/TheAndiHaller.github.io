9abaf5bc7465f485172e0de72e0ae11a78a9dfb8
Andi <andi_haller@hotmail.com>
2023-03-01 15:05:57 -0300

    adding translations and localstorage of theme and lang

---
+++ js/language.js
@@ -0,0 +1,46 @@
+const lang = {
+    "nav-btn-home": {
+      "en": "Home",
+      "es": "Inicio"
+    },
+    "nav-btn-projects": {
+      "en": "Projects",
+      "es": "Proyectos"
+    },
+    "nav-btn-projects": {
+      "en": "Projects",
+      "es": "Proyectos"
+    },
+    "nav-btn-blog": {
+      "en": "Blog",
+      "es": "Blog"
+    },
+    "nav-btn-resume": {
+      "en": "Resume",
+      "es": "CV"
+    },
+    "presentation-h1-hi": {
+      "en": "Hi! I'm Andreas",
+      "es": "Hola! Soy Andreas"
+    },
+    "presentation-h1-iam": {
+      "en": "I'm Andreas",
+      "es": "Soy Andreas"
+    },
+    "aboutme-text-1": {
+      "en": "I am a <span class=\"yellow-font\">software developer</span> with a passion for creating innovative and user-friendly solutions. I have a diverse background in programming, machining, customer service, education and entrepreneurship that has given me a broad range of skills and experiences.",
+      "es": "Soy un <span class=\"yellow-font\">desarrollador de software</span> apasionado por crear soluciones innovadoras y fáciles de usar. Tengo una formación diversa en programación, mecanizado, servicio al cliente, educación y emprendimiento, lo que me ha proporcionado una amplia variedad de habilidades y experiencias."
+    },
+    "aboutme-text-2": {
+      "en": "Now, as a graduate of a full stack web dev bootcamp, I am focused on taking my programming skills to the next level and landing my first job in a tech company. My diverse background has given me a unique perspective that allows me to approach challenges with creativity and innovation, and I am excited to bring this mindset to the world of software engineering.",
+      "es": "Ahora, como graduado de un bootcamp de desarrollo web Full Stack, estoy enfocado en llevar mis habilidades de programación al siguiente nivel y conseguir mi primer trabajo en una empresa tecnológica. Mi formación diversa me ha brindado una perspectiva única que me permite abordar los desafíos con creatividad e innovación, y estoy emocionado de llevar esta mentalidad al mundo de la ingeniería de software."
+    },
+    "skills-title": {
+      "en": "Skills",
+      "es": "Tecnologías"
+    },
+    "latestprojects-title": {
+      "en": "Latest Projects",
+      "es": "Proyectos recientes"
+    },    
+  }
--- js/main.js
+++ js/main.js
@@ -1,20 +1,83 @@
-// Dark Light switch
 const switchModeBtn = document.getElementById("cb1");
+const switchLanguageBtn = document.getElementById("switchLang");
 const body = document.body;
 const btnIcon = document.getElementById("nav-button-icon");
+let root = document.documentElement;
+let theme = localStorage.getItem("theme");
+let language = localStorage.getItem("language");
 
-switchModeBtn.addEventListener("click", function() {
-  body.classList.toggle("day-background");
-  //btnIcon.classList.toggle("bx bx-moon bx-spin");
-  console.log(btnIcon.classList.contains("bx-sun"));
-  if (btnIcon.classList.contains("bx-sun")) {
-    btnIcon.classList.remove("bx-sun");
-    btnIcon.classList.add("bx-moon");
+if (language) {
+  updateText(language);
+} else {
+  language = getLanguage();
+  updateText(language);
+}
+
+if (theme) {
+  switchTheme();
+} else {
+  if (window.matchMedia) {
+    // Check if the dark-mode Media-Query matches
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      theme = "dark";
+      switchTheme();
+    } else {
+      theme = "light";
+      switchTheme();
+    }
   } else {
+    theme = "light"; // default to light
+  }
+}
+
+function switchTheme() {
+  if (theme === "dark") {
+    root.style.setProperty("--background-color", "#11161e");
+    root.style.setProperty("--text-color", "#eaebeb");
+    root.style.setProperty(
+      "--navbar-resume-btn-bg",
+      "rgba(255, 255, 255, 0.2)"
+    );
+    root.style.setProperty("--navbar-bg-contrast", "rgba(33, 37, 41, 0.95)");
+
     btnIcon.classList.remove("bx-moon");
     btnIcon.classList.add("bx-sun");
 
+    localStorage.setItem("theme", "dark");
+  } else if (theme === "light") {
+    root.style.setProperty("--background-color", "#eaebeb");
+    root.style.setProperty("--text-color", "#11161e");
+    root.style.setProperty("--navbar-resume-btn-bg", "rgba(20, 0, 0, 0.1)");
+    root.style.setProperty("--navbar-bg-contrast", "rgba(255, 255, 255, 0.95)");
+
+    btnIcon.classList.remove("bx-sun");
+    btnIcon.classList.add("bx-moon");
+
+    localStorage.setItem("theme", "light");
   }
+}
+
+switchLanguageBtn.addEventListener("click", function () {
+  if (language === "es") {
+    language = "en";
+    updateText(language);
+    localStorage.setItem("language", language);
+    switchLanguageBtn.innerHTML = "EN";
+  } else {
+    language = "es";
+    updateText(language);
+    localStorage.setItem("language", language);
+    switchLanguageBtn.innerHTML = "ES";
+  }
+});
+
+switchModeBtn.addEventListener("click", function () {
+  if (theme === "dark") {
+    theme = "light";
+  } else if (theme === "light") {
+    theme = "dark";
+  }
+  switchTheme();
 });
 
 // Navbar
@@ -33,3 +96,21 @@ navbarToggle.addEventListener("click", toggleNavbarVisibility);
 navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
 navbarMenu.addEventListener("click", toggleNavbarVisibility);
 
+function getLanguage() {
+  const userLang = navigator.language;
+  const langCode = userLang.split("-")[0]; // extract the language code from the user's preference
+  return langCode;
+}
+
+function updateText(language) {
+  if (language === "en" || language === "es") {
+    const elements = document.querySelectorAll("[lang]"); // find all elements with a "data-lang" attribute
+    elements.forEach((el) => {
+      const id = el.id; // get the ID of the current element
+      if (id !== "html") {
+        const text = lang[id][language]; // get the appropriate translation for the current language
+        el.innerHTML = text; // update the text content of the current element
+      }
+    });
+  }
+}
