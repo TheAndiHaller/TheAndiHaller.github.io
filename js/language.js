3bbee4148b7e02652a3663e6f29cef302d14691e
Andi <andi_haller@hotmail.com>
2023-03-01 21:18:04 -0300

    added tridibot spanish

--- js/language.js
+++ js/language.js
@@ -50,5 +50,49 @@ const lang = {
     "resume-url": {
       "en": "https://github.com/TheAndiHaller/TheAndiHaller.github.io/raw/9abaf5bc7465f485172e0de72e0ae11a78a9dfb8/documents/Andreas%20Haller%20Full%20Stack%20Web%20Developer%20resume.pdf",
       "es": "https://github.com/TheAndiHaller/TheAndiHaller.github.io/raw/9abaf5bc7465f485172e0de72e0ae11a78a9dfb8/documents/Andreas%20Haller%20Full%20Stack%20Web%20Developer%20CV.pdf"
-    },      
+    },
+    "tridibot-s-1": {
+      "en": "Meet Tridibot: The 3D Printed Robot That You Can Control Over Wifi",
+      "es": "Conoce a Tridibot: El robot impreso en 3D que puedes controlar por wifi"
+    },   
+    "tridibot-s-2": {
+      "en": "The Origin of Tridibot",
+      "es": "El origen de Tridibot"
+    },    
+    "tridibot-p-1": {
+      "en": "Have you ever dreamed of having your own robot companion? A robot that can walk and even play soccer? Well, dream no more! In this blog post, I will introduce you to Tridibot, a 3D printed robot that is controlled over wifi from any device. Tridibot is a fun and educational project that anyone can build and customize. Let me tell you how it all started.",
+      "es": "¿Alguna vez has soñado con tener tu propio compañero robot? ¿Un robot que pueda caminar e incluso jugar al fútbol? ¡Pues no sueñes más! En este blog te voy a presentar a Tridibot, un robot impreso en 3D que se controla por wifi desde cualquier dispositivo. Tridibot es un proyecto divertido y educativo que cualquiera puede construir y personalizar. Déjame contarte cómo empezó todo."
+    },    
+    "tridibot-p-2": {
+      "en": "Tridibot was born out of a challenge. My friend Konrad and I are both passionate about robotics and 3D printing. We love to tinker with different gadgets and create new things. One day, we were contacted by a teacher from the University of La Punta, who asked us if we could build a robot that could help him in his classroom. He wanted a robot that could interact with his students, teach them some basic concepts of robotics and programming, and also entertain them with some games.",
+      "es": "Tridibot nació de un desafío. Mi amigo Konrad y yo somos apasionados de la robótica y la impresión 3D. Nos encanta trastear con diferentes gadgets y crear cosas nuevas. Un día, nos contactó un profesor de la Universidad de La Punta, que nos preguntó si podíamos construir un robot que le ayudara en su clase. Quería un robot que pudiera interactuar con sus alumnos, enseñarles algunos conceptos básicos de robótica y programación, y también entretenerlos con algunos juegos."
+    },    
+    "tridibot-p-3": {
+      "en": "We accepted the challenge and started brainstorming ideas. We decided to make a robot that had four legs instead of wheels because we thought it would be more agile and expressive. We also wanted to make it wireless so that it could be controlled from any device without cables or wires. And we wanted to make it affordable and accessible so that anyone could replicate it with common materials and tools.",
+      "es": "Aceptamos el desafío y empezamos a pensar ideas. Decidimos hacer un robot que tuviera cuatro patas en lugar de ruedas porque pensamos que sería más ágil y expresivo. También queríamos hacerlo inalámbrico para que se pudiera controlar desde cualquier dispositivo sin cables ni hilos. Y queríamos hacerlo asequible y accesible para que cualquiera pudiera replicarlo con materiales y herramientas comunes."
+    },    
+    "tridibot-p-4": {
+      "en": "We chose to use an ESP8266 and an Arduino nano as the micro-controllers for Tridibot. The ESP8266 is a tiny board that has wifi capabilities and can communicate with any device over the internet. The Arduino nano is a small board that can control servos and sensors. We connected them using serial communication and wrote the code using Arduino IDE.",
+      "es": "Elegimos usar un ESP8266 y un Arduino nano como los microcontroladores para Tridibot. El ESP8266 es una placa que tiene capacidades wifi y puede comunicarse con cualquier dispositivo por internet. El Arduino nano es una placa pequeña que puede controlar servos y sensores. Los conectamos usando comunicación serial y escribimos el código usando Arduino IDE."
+    },    
+    "tridibot-p-5": {
+      "en": "We wanted Tridibot to be rechargeable so we used a lithium-ion battery pack and a charging module. We also added an on/off switch and an LED RGB ring for feedback.",
+      "es": "Queríamos que Tridibot fuera recargable así que usamos una batería de litio-ion y un módulo de carga. También añadimos un interruptor de encendido/apagado y un aro de LEDs  RGB para retroalimentación."
+    },    
+    "tridibot-p-6": {
+      "en": "The best part about Tridibot is that it is open source. That means anyone can download the files, print their own parts, assemble their own robot, and modify it as they wish. We have uploaded all the files on GitHub and Thingiverse along with detailed instructions on how to build your own Tridibot.",
+      "es": "La mejor parte de Tridibot es que es de código abierto. Eso significa que cualquiera puede descargar los archivos, imprimir sus propias piezas, montar su propio robot y modificarlo como quiera. Hemos subido todos los archivos en GitHub y Thingiverse junto con instrucciones detalladas sobre cómo construir tu propio Tridibot."
+    },    
+    "tridibot-p-7": {
+      "en": "Tridibot is more than just a toy. It is a learning platform that can teach you about robotics, electronics, programming, 3D printing, and more. It is also a fun way to interact with your friends or family over wifi. You can control your Tridibot from your phone, tablet, laptop, or any other device that has a web browser.",
+      "es": "Tridibot es más que un juguete. Es una plataforma de aprendizaje que te puede enseñar sobre robótica, electrónica, programación, impresión 3D y más. También es una forma divertida de interactuar con tus amigos o familiares por wifi. Puedes controlar tu Tridibot desde tu teléfono, tableta, portátil o cualquier otro dispositivo que tenga un navegador web."
+    },    
+    "tridibot-p-8": {
+      "en": "We are very proud of what we have created and we hope you will enjoy it too. If you want to learn more about Tridibot or build your own please visit the Github repository.",
+      "es": "Estamos muy orgullosos de lo que hemos creado y esperamos que tú también lo disfrutes. Si quieres saber más sobre Tridibot o construir uno, por favor visita nuestro repositorio de Github."
+    },    
+    "tridibot-p-9": {
+      "en": "Thank you for reading this blog post and stay tuned for more updates on our project!",
+      "es": "¡Gracias por leer este blog y mantente atento a más novedades sobre nuestro proyecto!"
+    },  
   }
