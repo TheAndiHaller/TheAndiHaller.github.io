09d5f93f8f74b082cf0d91927f5cd92363f0fc80
Andi <andi_haller@hotmail.com>
2023-03-08 18:04:53 -0300

    changed a translation

--- js/language.js
+++ js/language.js
@@ -29,11 +29,11 @@ const lang = {
   },
   "aboutme-text-1": {
     en: 'I am a <span class="yellow-font">software developer</span> with a passion for creating innovative and user-friendly solutions. I have a diverse background in programming, machining, customer service, education and entrepreneurship that has given me a broad range of skills and experiences.',
-    es: 'Soy un <span class="yellow-font">desarrollador de software</span> apasionado por crear soluciones innovadoras y fáciles de usar. Tengo una formación diversa en programación, mecanizado, servicio al cliente, educación y emprendimiento, lo que me ha proporcionado una amplia variedad de habilidades y experiencias.',
+    es: 'Soy un <span class="yellow-font">desarrollador de software</span> apasionado por crear soluciones innovadoras y fáciles de usar. Tengo una trayectoria diversa en programación, mecanizado, servicio al cliente, educación y emprendimiento, lo que me ha proporcionado una amplia variedad de habilidades y experiencias.',
   },
   "aboutme-text-2": {
     en: "Now, as a graduate of a full stack web dev bootcamp, I am focused on taking my programming skills to the next level and landing my first job in a tech company. My diverse background has given me a unique perspective that allows me to approach challenges with creativity and innovation, and I am excited to bring this mindset to the world of software engineering.",
-    es: "Ahora, como graduado de un bootcamp de desarrollo web Full Stack, estoy enfocado en llevar mis habilidades de programación al siguiente nivel y conseguir mi primer trabajo en una empresa tecnológica. Mi formación diversa me ha brindado una perspectiva única que me permite abordar los desafíos con creatividad e innovación, y estoy emocionado de llevar esta mentalidad al mundo de la ingeniería de software.",
+    es: "Ahora, como graduado de un bootcamp de desarrollo web Full Stack, estoy enfocado en llevar mis habilidades de programación al siguiente nivel y conseguir mi primer trabajo en una empresa tecnológica. Mi trayectoria diversa me ha brindado una perspectiva única que me permite abordar los desafíos con creatividad e innovación, y estoy emocionado de llevar esta mentalidad al mundo de la ingeniería de software.",
   },
   "skills-title": {
     en: "Skills",
